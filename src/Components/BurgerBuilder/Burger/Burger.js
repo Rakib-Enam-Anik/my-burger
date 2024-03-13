df26f4f7392c88b2dd9847079535f772f9bd1850
Rakib Enam <rakibenamanik161@gmail.com>
2024-03-13 23:03:02 +0600

    3rd tutorial implementation Completed

--- src/Components/BurgerBuilder/Burger/Burger.js
+++ src/Components/BurgerBuilder/Burger/Burger.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import Ingredient from '../Ingredient/Ingredient';
 
 
 
 const Burger = props =>{
     return(
         <div>
-            <p> Burger </p>
+            <Ingredient type="bread-top" />
+            <Ingredient type="cheese" />
+            <Ingredient type="salad" />
+            <Ingredient type="meat" />
+            <Ingredient type="bread-bottom" />
         </div>
     )
 }
