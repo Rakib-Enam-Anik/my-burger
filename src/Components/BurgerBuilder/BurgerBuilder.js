7256222ed2c2e4b3d5793e206bc4572856804335
Rakib Enam <md.rakib.enam.anik@bracu.ac.bd>
2024-03-20 15:53:20 +0600

    Before Solving Error of tutorial 11 Creating Checkout Option

--- src/Components/BurgerBuilder/BurgerBuilder.js
+++ src/Components/BurgerBuilder/BurgerBuilder.js
@@ -73,7 +73,7 @@ class BurgerBuilder extends Component {
             <Summary ingredients={this.props.ingredients} />
           </ModalBody>
           <ModalFooter>
-            <Button color="success" onClick={this.handleCheckout}>
+            <Button  style={{ backgroundColor:"#D70F64"}} onClick={this.handleCheckout}>
               Continue to Checkout
             </Button>
             <Button color="secondary" onClick={this.toggleModal}>
--- src/Components/Orders/Checkout/Checkout.js
+++ src/Components/Orders/Checkout/Checkout.js
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, {Component} from 'react';
+import {Button} from 'reactstrap';
+class Checkout extends Component  {
+    state = {
+        values: {
+            deliveryAddress: "",
+            phone: "",
+            paymentType: "Cash On Delivery",
+        }
+    }
+     
+    goBack = () => {
+        this.props.history.goBack("/");
+    }
 
-const Checkout = () => {
-    return (
-        <div>Checkout</div>
-    )
+    inputChangerHandler = (e) => {
+        this.setState({
+           values: {
+            ...this.state.values,
+            [e.target.name]: e.target.value,
+           }
+        })
+    }
+
+    submitHandler = () => {
+        console.log(this.state.values);
+    }
+
+    render() {
+        return (
+            <div>
+                <form style={{
+                    border: "1px solid grey",
+                    boxShadow: "1px 1px #888888",
+                    padding: "20px",
+                    margin: "20px",
+                }}>
+                    <textarea name="deliveryAddress" value={this.state.values.deliveryAddress}
+                    className='form-control' placeholder="Your Delivery Address"
+                    onChange={(e)=> this.inputChangerHandler(e)}></textarea>
+                    <br />
+                    <input name="phone" className="form-control" value={this.state.values.phone} 
+                    placeholder="Your Phone Number" onChange={(e)=> this.inputChangerHandler(e)} />
+                    <br />
+                    <select name="paymentType" className="form-control" value={this.state.values.paymentType}
+                    onChange={(e)=> this.inputChangerHandler(e)}>
+                    <option value="Cash On Delivery">Cash On Delivery</option>
+                    <option value="Bkash">Bkash</option>
+                    </select>
+                    <br />
+                    <Button style={{backgroundColor:"#D70F64"}} className="mr-auto" onClick={this.submitHandler}> Place Order </Button>
+                    <Button color="secondary" className="ml-1" onClick={this.goBack}> Cancel </Button>
+                </form>
+            </div>
+        )
+        
+    }
 }
 
-export default Checkout
+export default Checkout;
