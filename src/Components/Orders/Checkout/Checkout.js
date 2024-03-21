a0895d1a9f5ee4beede0c122ae03513d0e1d6f4e
Rakib Enam <md.rakib.enam.anik@bracu.ac.bd>
2024-03-21 14:21:26 +0600

    Before Solving goback cancel issue

--- src/Components/Orders/Checkout/Checkout.js
+++ src/Components/Orders/Checkout/Checkout.js
@@ -1,5 +1,22 @@
 import React, {Component} from 'react';
 import {Button} from 'reactstrap';
+import {connect} from 'react-redux';
+import { useNavigate } from "react-router-dom";
+
+
+
+import axios from 'axios';
+
+
+const mapStateTopProps = state => {
+    return {
+        ingredients: state.ingredients,
+        totalPrice: state.totalPrice,
+        purchasable: state.purchasable,
+    }
+}
+
+
 class Checkout extends Component  {
     state = {
         values: {
@@ -10,7 +27,10 @@ class Checkout extends Component  {
     }
      
     goBack = () => {
-        this.props.history.goBack("/");
+        
+        //this.props.history.goBack("/");
+        const navigate = useNavigate();
+        navigate("-1");
     }
 
     inputChangerHandler = (e) => {
@@ -23,12 +43,28 @@ class Checkout extends Component  {
     }
 
     submitHandler = () => {
-        console.log(this.state.values);
+        const order = {
+            ingredients: this.props.ingredients,
+            customer: this.state.values,
+            price: this.props.totalPrice,
+            orderTime: new Date(),
+        }
+        axios.post("https://burger-builder-40852-default-rtdb.firebaseio.com/orders.json", order)
+        .then(response=> console.log(response))
+        .catch(err => console.log(err))
+       
     }
 
     render() {
         return (
             <div>
+
+                <h4 style={{
+                    border: "1px solid grey",
+                    boxShadow: "1px 1px #888888",
+                    padding: "20px",
+                    margin: "20px",
+                }}>Payment:{this.props.totalPrice} BDT </h4>
                 <form style={{
                     border: "1px solid grey",
                     boxShadow: "1px 1px #888888",
@@ -57,4 +93,5 @@ class Checkout extends Component  {
     }
 }
 
-export default Checkout;
+export default connect(mapStateTopProps)(Checkout);
+
