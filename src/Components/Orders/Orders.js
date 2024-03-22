f43a960cc2c3df38eb1f0a1c78041252dabf5548
Rakib Enam <md.rakib.enam.anik@bracu.ac.bd>
2024-03-22 15:56:03 +0600

    Implemented Fetching the Orders Tutorial 14

--- src/Components/Orders/Orders.js
+++ src/Components/Orders/Orders.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, {Component} from 'react';
+import {connect} from'react-redux';
+import {fetchOrders} from '../../redux/actionCreators';
 
-const Orders = () => {
-    return (
-        <div>Order</div>
-    )
+const mapStateToProps = state => {
+    return {
+        orders: state.orders,
+        orderLoading: state.orderLoading,
+        orderErr: state.orderErr,
+    }
+}
+const mapDispatchToProps = dispatch => {
+    return {
+        fetchOrders: () => dispatch(fetchOrders()),
+    }
+}
+class Orders extends Component {
+    componentDidMount() {
+        this.props.fetchOrders();
+    }
+    componentDidUpdate() {
+        console.log(this.props);
+    }
+    render() {
+        return (
+            <div>Order</div>
+        )
+    }
+   
 }
 
-export default Orders
+export default connect(mapStateToProps, mapDispatchToProps )(Orders);
